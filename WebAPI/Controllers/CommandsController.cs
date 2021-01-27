c5d0639d3e913fe52d7b0c46a7d86f8481fe2c8a
Milos.Gak <Milos.Gak@BLL-MILOS-GAK>
2021-01-27 17:51:32 +0100

    Add project files.

---
+++ WebAPI/Controllers/CommandsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+
+        //GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+            return Ok(commandItems);
+        }
+        //GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int Id)
+        {
+            var commandItem = _repository.GetCommandById(Id);
+            return Ok(commandItem);
+
+        }
+    }
+}
---
+++ WebAPI/Data/MockCommanderRepo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                 new Command { Id = 0, HowTo = "This Way!", Line = "Line String", Platform = "Platform String" },
+                 new Command { Id = 1, HowTo = "This Way2!", Line = "Line String2", Platform = "2Platform String" },
+                 new Command { Id = 2, HowTo = "This Way3!", Line = "Line String3", Platform = "3Platform String" },
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int Id)
+        {
+            return new Command { Id = 0, HowTo = "This Way!", Line = "Line String", Platform = "Platform String" };
+        }
+    }
+}
